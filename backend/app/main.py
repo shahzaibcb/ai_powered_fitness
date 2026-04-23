36542ce8d9a8772600904eb57d22793b86747391
MS DEPLOY <voltron+ms-deploy@10up.com>
2026-04-23 16:29:44 +0500

    deploy ingress-nginx

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Nutrition AI API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://35.227.120.41"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
